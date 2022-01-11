d2ae5945aeda9480a1324b53ab02a3b10be119de
Phil G <fillupgalbraith@gmail.com>
2022-01-11 14:21:31 -0800

    serving no endpoints

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
+const express = require('express')
+
+const server = express()
+
+const port = 8000
+
+
+server.get('/', (req, res) => {
+  res.send('Running Smoothe!')
+})
+  
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
+
 // require your posts router and connect it here
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 // require your server and launch it here
+
+const server = require('./api/server')
