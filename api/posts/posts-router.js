08313719bc0898767c6f6f1eaef9e507493a47a1
Phil G <fillupgalbraith@gmail.com>
2022-01-11 18:11:02 -0800

    passing five tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,65 @@
 // implement your posts router here
+const express = require('express')
+
+const {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    findPostComments,
+    findCommentById,
+    insertComment,
+  } = require('./posts-model')
+
+const router = express.Router()
+
+router.get('/:id/comments', (req, res) => {
+    res.send('just the comments of one id!')
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const idPost = await findById(id)
+    try { 
+      if ((typeof idPost) !== 'undefined') {
+        res.status(200).json({ ...idPost})
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+      }
+    } 
+    catch {
+      res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+})
+
+router.get('/', async (req, res) => {
+  const posts = await find()
+  try { 
+    res.status(200).json(posts)
+  } catch {
+    res.status(500).json({ message: "The posts information could not be retrieved" })
+  }
+})
+  
+router.post('/', async (req, res) => {
+    try {
+        if (!req.body.bio || !req.body.name) {
+          res.status(400).json({message: "Please provide title and contents for the post"})
+        } else {
+          const {id} = await insert(req.body)
+          const newPost = await findById(id)
+          res.status(201).json(newPost)
+        }
+      } catch(err) {
+        res.status(500).json({ message: "There was an error while saving the post to the database" })
+      }
+})
+
+router.get('*', (req, res) => {
+  res.status(404).json({ message: "The post with the specified ID does not exist" })
+})
+  
+
+// require your posts router and connect it here
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,15 +1,17 @@
 // implement your server here
 const express = require('express')
+const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 const port = 8000
 
+server.use('/api/posts', postsRouter)
 
-server.get('/', (req, res) => {
-  res.send('Running Smoothe!')
+server.get('*', (req, res) => {
+  res.status(404).json({ message: "The post with the specified ID does not exist" })
 })
-  
+
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
